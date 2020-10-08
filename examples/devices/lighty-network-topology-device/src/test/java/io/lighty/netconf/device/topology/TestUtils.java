77bcc0ac601769b9f36530253b5f71acaa0b2dc5
Peter Valka <Peter.Valka@pantheon.tech>
2020-10-08 14:21:09 +0200

    Review changes
    add CRUD operations on netconf-node-topology move assertions to the tests directly
    Signed-off-by: Peter Valka <Peter.Valka@pantheon.tech>

--- examples/devices/lighty-network-topology-device/src/test/java/io/lighty/netconf/device/topology/TestUtils.java
+++ examples/devices/lighty-network-topology-device/src/test/java/io/lighty/netconf/device/topology/TestUtils.java
@@ -5,7 +5,6 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at https://www.eclipse.org/legal/epl-v10.html
  */
-
 package io.lighty.netconf.device.topology;
 
 import java.io.File;
