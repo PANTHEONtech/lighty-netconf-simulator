d0cd3446b103d53d6e945f9059cdfa86688c979a
Martin Bugan <martin.bugan@pantheon.tech>
2021-01-21 15:49:11 +0100

    Introduce new 13.1.2-SNAPSHOT version
    Signed-off-by: Martin Bugan <martin.bugan@pantheon.tech>

--- examples/devices/lighty-actions-device/src/assembly/resources/start-device.sh
+++ examples/devices/lighty-actions-device/src/assembly/resources/start-device.sh
@@ -6,7 +6,7 @@
 #
 # When run without a parameter a default port 17830 will be used.
 
-CLASSPATH=lighty-action-device-13.1.1.jar
+CLASSPATH=lighty-action-device-13.1.2-SNAPSHOT.jar
 
 for jar in `ls -1 lib/`;
 do
