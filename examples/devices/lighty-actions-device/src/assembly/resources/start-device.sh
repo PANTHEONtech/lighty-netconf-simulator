26c4af20190126192bfc4a1aae30b1974ace09f8
Peter Suna <peter.suna@pantheon.tech>
2021-03-19 12:00:03 +0100

    Bump versions to Silicon
     - Used SNAPSHOT version at lighty.core for
       test purpose. Change when release version will be available.
    
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- examples/devices/lighty-actions-device/src/assembly/resources/start-device.sh
+++ examples/devices/lighty-actions-device/src/assembly/resources/start-device.sh
@@ -6,7 +6,7 @@
 #
 # When run without a parameter a default port 17830 will be used.
 
-CLASSPATH=lighty-action-device-13.2.1-SNAPSHOT.jar
+CLASSPATH=lighty-action-device-14.0.0.jar
 
 for jar in `ls -1 lib/`;
 do
