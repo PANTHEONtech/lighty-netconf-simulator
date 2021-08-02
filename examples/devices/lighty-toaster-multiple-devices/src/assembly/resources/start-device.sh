4fa51869877f97c6d5980aa26d8f879de4fce3a6
PeterSuna <Peter.Suna@pantheon.tech>
2021-08-02 11:25:34 +0200

    Bump to 14.1.0 version
    Signed-off-by: PeterSuna <Peter.Suna@pantheon.tech>

--- examples/devices/lighty-toaster-multiple-devices/src/assembly/resources/start-device.sh
+++ examples/devices/lighty-toaster-multiple-devices/src/assembly/resources/start-device.sh
@@ -15,7 +15,7 @@
 # ./start-device --starting-port 20000 --device-count 200 --thread-pool-size 200
 #
 
-CLASSPATH=lighty-toaster-multiple-devices-14.1.0-SNAPSHOT.jar
+CLASSPATH=lighty-toaster-multiple-devices-14.1.0.jar
 
 ARGUMENT_LIST=(
     "device-count"
