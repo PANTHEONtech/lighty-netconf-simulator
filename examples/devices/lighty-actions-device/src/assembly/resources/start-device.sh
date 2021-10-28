5e92073496aa71c2978ad79e00f329ada32b9c36
Peter Suna <peter.suna@pantheon.tech>
2021-10-28 13:47:13 +0200

    Fix resources versions to 14.2.1-SNAPSHOT
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- examples/devices/lighty-actions-device/src/assembly/resources/start-device.sh
+++ examples/devices/lighty-actions-device/src/assembly/resources/start-device.sh
@@ -6,7 +6,7 @@
 #
 # When run without a parameter a default port 17830 will be used.
 
-CLASSPATH=lighty-action-device-14.2.0.jar
+CLASSPATH=lighty-action-device-14.2.1-SNAPSHOT.jar
 
 for jar in `ls -1 lib/`;
 do
