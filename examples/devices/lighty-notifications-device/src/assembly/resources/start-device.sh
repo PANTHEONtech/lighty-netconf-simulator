f06ec05a6b4f651d2737915a7f1641f63dbcd229
Peter Suna <peter.suna@pantheon.tech>
2022-02-24 13:42:37 +0100

    Prepare release 14.4.0
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- examples/devices/lighty-notifications-device/src/assembly/resources/start-device.sh
+++ examples/devices/lighty-notifications-device/src/assembly/resources/start-device.sh
@@ -6,7 +6,7 @@
 #
 # When run without a parameter a default port 17830 will be used.
 
-CLASSPATH=lighty-notifications-device-14.3.1-SNAPSHOT.jar
+CLASSPATH=lighty-notifications-device-14.4.0.jar
 
 for jar in `ls -1 lib/`;
 do
