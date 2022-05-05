399d2bd9a2d632799028b1725f3fc081871407d1
Peter Suna <peter.suna@pantheon.tech>
2022-05-05 12:42:36 +0200

    Bump simulator versions to 16.0.0-SNAPSHOT
    Signed-off-by: Peter Suna <peter.suna@pantheon.tech>

--- examples/devices/lighty-notifications-device/src/assembly/resources/start-device.sh
+++ examples/devices/lighty-notifications-device/src/assembly/resources/start-device.sh
@@ -6,7 +6,7 @@
 #
 # When run without a parameter a default port 17830 will be used.
 
-CLASSPATH=lighty-notifications-device-15.2.1-SNAPSHOT.jar
+CLASSPATH=lighty-notifications-device-16.0.0-SNAPSHOT.jar
 
 for jar in `ls -1 lib/`;
 do
