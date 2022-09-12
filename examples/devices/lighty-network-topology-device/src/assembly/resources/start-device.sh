5c4ba999de8e3b69d5fb37cf6755d1ea31bb9e96
tobias.pobocik <tobias.pobocik@pantheon.tech>
2022-09-12 10:54:37 +0200

    Prepare next dev 16.2.0-SPANSKOT
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- examples/devices/lighty-network-topology-device/src/assembly/resources/start-device.sh
+++ examples/devices/lighty-network-topology-device/src/assembly/resources/start-device.sh
@@ -6,7 +6,7 @@
 #
 # When run without a parameter a default port 17830 will be used.
 
-CLASSPATH=lighty-network-topology-device-16.1.0.jar
+CLASSPATH=lighty-network-topology-device-16.2.0-SNAPSHOT.jar
 
 for jar in `ls -1 lib/`;
 do
