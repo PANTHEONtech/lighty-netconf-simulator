157b4ca354694745c913365c85acc0223f7652ee
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2022-10-21 15:19:59 +0200

    Bump to 18.0.0-SNAPSHOT
    Start next development iteration toward compatibility
    release with Lighty.io 18.
    
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- examples/devices/lighty-network-topology-device/src/assembly/resources/start-device.sh
+++ examples/devices/lighty-network-topology-device/src/assembly/resources/start-device.sh
@@ -6,7 +6,7 @@
 #
 # When run without a parameter a default port 17830 will be used.
 
-CLASSPATH=lighty-network-topology-device-17.0.0.jar
+CLASSPATH=lighty-network-topology-device-18.0.0-SNAPSHOT.jar
 
 for jar in `ls -1 lib/`;
 do
