3b0c8018828ede7f1cbb95b281a39a9fb386d4ae
tobias.pobocik <tobias.pobocik@pantheon.tech>
2023-02-03 09:55:54 +0100

    Bump to 17.3.0-SNAPSHOT
    Start next development iteration towards to compatibility
    release with Lighty.io 17.3.0.
    
    JIRA: LIGHTY-160
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- examples/devices/lighty-actions-device/src/assembly/resources/start-device.sh
+++ examples/devices/lighty-actions-device/src/assembly/resources/start-device.sh
@@ -6,7 +6,7 @@
 #
 # When run without a parameter a default port 17830 will be used.
 
-CLASSPATH=lighty-action-device-17.2.0.jar
+CLASSPATH=lighty-action-device-17.3.0-SNAPSHOT.jar
 
 for jar in `ls -1 lib/`;
 do
