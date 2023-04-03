d7a76e91006c60ad57887659566baa2d16529bdf
tobias.pobocik <tobias.pobocik@pantheon.tech>
2023-04-03 12:56:17 +0200

    Bump to 19.0.0-SNAPSHOT
    Start next development iteration toward compatibility
    release with Lighty.io 19.
    
    JIRA:LIGHTY-197
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- examples/devices/lighty-toaster-multiple-devices/src/assembly/resources/start-device.sh
+++ examples/devices/lighty-toaster-multiple-devices/src/assembly/resources/start-device.sh
@@ -15,7 +15,7 @@
 # ./start-device --starting-port 20000 --device-count 200 --thread-pool-size 200
 #
 
-CLASSPATH=lighty-toaster-multiple-devices-18.0.0.jar
+CLASSPATH=lighty-toaster-multiple-devices-19.0.0-SNAPSHOT.jar
 
 ARGUMENT_LIST=(
     "device-count"
