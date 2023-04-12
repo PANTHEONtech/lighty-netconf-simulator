d1002f25718889a5c97556c0f152c22b168c3b84
tobias.pobocik <tobias.pobocik@pantheon.tech>
2023-04-12 13:54:48 +0200

    Prepare next dev phase 16.5.0-SNAPSHOT
    JIRA:LIGHTY-216
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- examples/devices/lighty-notifications-device/src/assembly/resources/start-device.sh
+++ examples/devices/lighty-notifications-device/src/assembly/resources/start-device.sh
@@ -6,7 +6,7 @@
 #
 # When run without a parameter a default port 17830 will be used.
 
-CLASSPATH=lighty-notifications-device-16.4.0.jar
+CLASSPATH=lighty-notifications-device-16.5.0-SNAPSHOT.jar
 
 for jar in `ls -1 lib/`;
 do
