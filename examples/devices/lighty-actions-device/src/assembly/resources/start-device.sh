37f9829574952582d10ee9388ca5d6c642b6b728
tobias.pobocik <tobias.pobocik@pantheon.tech>
2024-04-15 17:26:15 +0200

    Prepare release 20.0.0
    JIRA: LIGHTY-294
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- examples/devices/lighty-actions-device/src/assembly/resources/start-device.sh
+++ examples/devices/lighty-actions-device/src/assembly/resources/start-device.sh
@@ -6,7 +6,7 @@
 #
 # When run without a parameter a default port 17830 will be used.
 
-CLASSPATH=lighty-action-device-20.0.0-SNAPSHOT.jar
+CLASSPATH=lighty-action-device-20.0.0.jar
 
 for jar in `ls -1 lib/`;
 do
