8f62217ccc3cb3349075dcc7ed4a91273ebf61d1
Ivan Hrasko <ivan.hrasko@pantheon.tech>
2024-05-07 14:19:21 +0200

    Bump to 20.1.0-SNAPSHOT
    Start next development iteration towards 20.1.0.
    
    Signed-off-by: Ivan Hrasko <ivan.hrasko@pantheon.tech>

--- examples/devices/lighty-toaster-multiple-devices/src/assembly/resources/start-device.sh
+++ examples/devices/lighty-toaster-multiple-devices/src/assembly/resources/start-device.sh
@@ -15,7 +15,7 @@
 # ./start-device --starting-port 20000 --device-count 200 --thread-pool-size 200
 #
 
-CLASSPATH=lighty-toaster-multiple-devices-20.0.0.jar
+CLASSPATH=lighty-toaster-multiple-devices-20.1.0-SNAPSHOT.jar
 
 ARGUMENT_LIST=(
     "device-count"
