862dfac81c67457e733f3d8623fd0d63e2326db1
tobias.pobocik <tobias.pobocik@pantheon.tech>
2024-10-15 13:59:48 +0200

    Bump to 19.5.0-SNAPSHOT
    Start next development iteration.
    
    JIRA: LIGHTY-321
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- examples/devices/lighty-network-topology-device/src/assembly/resources/start-device.sh
+++ examples/devices/lighty-network-topology-device/src/assembly/resources/start-device.sh
@@ -6,7 +6,7 @@
 #
 # When run without a parameter a default port 17830 will be used.
 
-CLASSPATH=lighty-network-topology-device-19.4.0.jar
+CLASSPATH=lighty-network-topology-device-19.5.0-SNAPSHOT.jar
 
 for jar in `ls -1 lib/`;
 do
